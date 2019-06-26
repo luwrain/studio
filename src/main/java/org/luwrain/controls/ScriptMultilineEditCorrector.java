9876cf3a21e068b1537165a3bcfaf63c1a4fa101
Michael Pozhidaev <msp@luwrain.org>
2019-06-26 19:01:14 +0800

    Minor fixes

--- src/main/java/org/luwrain/controls/ScriptMultilineEditCorrector.java
+++ src/main/java/org/luwrain/controls/ScriptMultilineEditCorrector.java
@@ -9,66 +9,73 @@ import org.luwrain.util.*;
 
 import org.luwrain.controls.MultilineEdit2.ModificationResult;
 
-    public class ScriptMultilineEditCorrector implements MultilineEditCorrector2
+public class ScriptMultilineEditCorrector implements MultilineEditCorrector2
+{
+    protected final MultilineEditCorrector2 base;
+
+    public ScriptMultilineEditCorrector(MultilineEditCorrector2 base)
     {
-	public String getLine(int index)
-	{
-	    return "";
-	}
+	NullCheck.notNull(base, "base");
+	this.base = base;
+    }
 
-	public int getLineCount()
-	{
-	    return 0;
-	}
-	
-	public int getHotPointX()
-	{
-	    return 0;
-	}
-	
-	public int getHotPointY()
-	{
-	    return 0;
-	}
-	
-	public String getTabSeq()
-	{
-	    return null;
-	}
-	
-	public ModificationResult deleteChar(int pos, int lineIndex)
-	{
-	    return null;
-	}
-	
+    @Override public String getLine(int index)
+    {
+	return base.getLine(index);
+    }
 
-	public ModificationResult deleteRegion(int fromX, int fromY, int toX, int toY)
-	{
-	    return null;
-	}
+    @Override public int getLineCount()
+    {
+	return base.getLineCount();
+    }
 
-	public ModificationResult insertRegion(int x, int y, String[] lines)
-	{
-	    return null;
-	}
+    @Override public int getHotPointX()
+    {
+	return base.getHotPointX();
+    }
 
-	public ModificationResult putChars(int pos, int lineIndex, String str)
-	{
-	    return null;
-	}
+    @Override public int getHotPointY()
+    {
+	return base.getHotPointY();
+    }
 
-	public ModificationResult mergeLines(int firstLineIndex)
-	{
-	    return null;
-	}
+    @Override public String getTabSeq()
+    {
+	return base.getTabSeq();
+    }
 
-public ModificationResult splitLine(int pos, int lineIndex)
-	{
-	    return null;
-	}
+    @Override public ModificationResult deleteChar(int pos, int lineIndex)
+    {
+	return null;
+    }
 
-public ModificationResult doEditAction(TextEditAction action)
-	{
-	    return null;
-	}
+    @Override public ModificationResult deleteRegion(int fromX, int fromY, int toX, int toY)
+    {
+	return null;
+    }
+
+    @Override public ModificationResult insertRegion(int x, int y, String[] lines)
+    {
+	return null;
+    }
+
+    @Override public ModificationResult putChars(int pos, int lineIndex, String str)
+    {
+	return null;
+    }
+
+    @Override public ModificationResult mergeLines(int firstLineIndex)
+    {
+	return null;
+    }
+
+    @Override public ModificationResult splitLine(int pos, int lineIndex)
+    {
+	return null;
+    }
+
+    @Override public ModificationResult doEditAction(TextEditAction action)
+    {
+	return null;
+    }
     }
