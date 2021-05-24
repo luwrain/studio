fc837410aa56e2e013c774366bfebdc3557ac46d
Michael Pozhidaev <msp@luwrain.org>
2021-05-24 10:59:07 +0700

    Adding TexCompilation

---
+++ src/main/java/org/luwrain/studio/backends/tex/CompilationCommandFactory.java
@@ -0,0 +1,63 @@
+/*
+   Copyright 2012-2021 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.studio.backends.tex;
+
+import java.io.*;
+import java.util.*;
+
+import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.proxy.*;
+
+import org.luwrain.core.*;
+import org.luwrain.studio.*;
+
+final class CompilationCommandFactory
+{
+    ProxyExecutable newCompilationCommand(TexCompilation compilation, String commandName)
+    {
+	NullCheck.notNull(compilation, "compilation");
+	NullCheck.notEmpty(commandName, "commandName");
+	return (ProxyExecutable)(args)->{
+	    final List<String> argsArray = new ArrayList();
+	    for(Value v: args)
+	    {
+		if (v == null)
+		    throw new NullPointerException(commandName + ": Args can't include nulls");
+		argsArray.add(v.toString());
+	    }
+	    run(compilation, commandName, argsArray.toArray(new String[argsArray.size()]));
+	    return null;
+	};
+    }
+
+    private void run(TexCompilation compilation, String commandName, String[] args)
+    {
+	NullCheck.notNull(compilation, "compilation");
+	NullCheck.notEmpty(commandName, "commandName");
+	NullCheck.notNullItems(args, "args");
+	final List<String> cmd = new ArrayList();
+	cmd.add(commandName);
+	cmd.addAll(Arrays.asList(args));
+	try {
+	final Process p = new ProcessBuilder(cmd).directory(compilation.getProjectDir()).start();
+	}
+	catch(IOException e)
+	{
+	    throw new CompilationException(e);
+	}
+    }
+}
---
+++ src/main/java/org/luwrain/studio/backends/tex/TexCompilation.java
@@ -0,0 +1,56 @@
+/*
+   Copyright 2012-2021 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.studio.backends.tex;
+
+import java.io.*;
+import java.util.*;
+
+import org.luwrain.core.*;
+import org.luwrain.core.script2.*;
+import org.luwrain.studio.*;
+
+final class TexCompilation
+{
+    private final IDE ide;
+    private final TexProject proj;
+
+    TexCompilation(IDE ide, TexProject proj)
+    {
+	NullCheck.notNull(ide, "ide");
+	NullCheck.notNull(proj, "proj");
+	this.ide = ide;
+	this.proj = proj;
+    }
+
+    void build()
+    {
+	runCompilationScript();
+    }
+
+    private void runCompilationScript()
+    {
+	final ScriptCore scriptCore = new ScriptCore(ide.getLuwrainObj(), (bindings)->{
+		final CompilationCommandFactory factory = new CompilationCommandFactory();
+		bindings.putMember("latex", factory.newCompilationCommand(TexCompilation.this, "latex"));
+	    });
+    }
+
+    File getProjectDir()
+    {
+	return new File("/tmp");
+    }
+}
