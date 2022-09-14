20e5dc324123b154dad6d1ea716235e6f78a5594
Michael Pozhidaev <msp@luwrain.org>
2022-09-15 05:57:01 +0700

    Adding proj.main

---
+++ src/main/java/org/luwrain/studio/proj/main/Folder.java
@@ -0,0 +1,150 @@
+/*
+   Copyright 2012-2022 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.studio.proj.main;
+
+import java.util.*;
+import java.io.*;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.annotations.*;
+
+import org.luwrain.core.*;
+import org.luwrain.core.events.*;
+import org.luwrain.studio.*;
+import org.luwrain.popups.*;
+
+import org.luwrain.studio.edit.tex.*;
+
+import static org.luwrain.studio.Part.*;
+
+public final class Folder implements Part
+{
+    private String name = "";
+    private List<Folder> subfolders = null;
+	    private List<TexSourceFile> texFiles = null;
+
+    private transient ProjectImpl proj = null;
+    private transient IDE ide = null;
+
+    void init(ProjectImpl proj, IDE ide)
+    {
+	NullCheck.notNull(proj, "proj");
+	NullCheck.notNull(ide, "ide");
+	this.proj = proj;
+	this.ide = ide;
+	if (name == null)
+	    name = "---";
+	if (subfolders == null)
+	    subfolders = new ArrayList<>();
+			if (texFiles == null)
+			    texFiles = new ArrayList<>();
+	    for(Folder f: subfolders)
+		f.init(proj, ide);
+	    for(TexSourceFile f: texFiles)
+		f.init(proj, ide);
+    }
+
+    @Override public Part [] getChildParts()
+    {
+	final List<Part> res = new ArrayList<>();
+	if (subfolders != null)
+	    for(Part p: subfolders)
+		res.add(p);
+	if (texFiles != null)
+	    for(Part p: texFiles)
+		res.add(p);
+	return res.toArray(new Part[res.size()]);
+    }
+
+
+    public void setName(String name)
+    {
+	NullCheck.notEmpty(name, "name");
+	this.name = name;
+    }
+
+
+public void setSubfolders(List<Folder> subfolders)
+{
+    NullCheck.notNull(subfolders, "subfolders");
+    this.subfolders = subfolders;
+}
+
+    public List<TexSourceFile> getTexFiles() { return this.texFiles != null?this.texFiles:new ArrayList<>(); }
+    /*
+    public void setSourceFiles(List<TexSourceFile> sourceFiles)
+    {
+	NullCheck.notNull(sourceFiles, "sourceFiles");
+	this.sourceFiles = sourceFiles;
+    }
+    */
+
+                @Override public org.luwrain.studio.Part.Action[] getActions()
+    {
+	return actions(
+		       action("Новый раздел", new InputEvent(InputEvent.Special.INSERT, EnumSet.of(InputEvent.Modifiers.CONTROL)), this::newSubfolder),
+		       action("Добавить файл", new InputEvent(InputEvent.Special.INSERT), this::newSourceFile)
+		       );
+    }
+
+        @Override public Editing startEditing() { return null; }
+    @Override public String getTitle() { return name; }
+    @Override public String toString() { return name; }
+    public String getName() { return this.name; }
+        public List<Folder> getSubfolders() { return this.subfolders != null?this.subfolders:new ArrayList<>(); }
+
+
+
+    private boolean newSubfolder(IDE ide)
+    {
+	/*
+	final String name = Popups.textNotEmpty(ide.getLuwrainObj(), proj.getStrings().newFolderPopupName(), proj.getStrings().newFolderPopupPrefix(), "");
+	if (name == null || name.trim().isEmpty())
+	    return true;
+	final TexFolder newFolder = new TexFolder();
+	newFolder.setName(name.trim());
+	newFolder.init(proj, ide);
+	this.subfolders.add(newFolder);
+	ide.onFoldersUpdate();
+	*/
+	return true;
+    }
+
+    private boolean newSourceFile(IDE ide)
+    {
+	NullCheck.notNull(ide, "ide");
+	/*
+	final String name = Popups.textNotEmpty(ide.getLuwrainObj(), proj.getStrings().newSourceFilePopupName(), proj.getStrings().newSourceFilePopupPrefix(), "");
+	if (name == null || name.trim().isEmpty())
+	    return true;
+	final File file = Popups.existingFile(ide.getLuwrainObj(), "Новый файл");
+	if (file == null)
+	    return true;
+	*/
+	final String name = Popups.textNotEmpty(ide.getLuwrainObj(), "Новый файл", "Имя:", "");
+	if (name == null)
+	    return true;
+	final String path = Popups.textNotEmpty(ide.getLuwrainObj(), "Новый файл", "Путь :", "");
+	if (path == null)
+	    return true;
+	final TexSourceFile newFile = new TexSourceFile(name, path);
+	newFile.init(proj, ide);
+	this.texFiles.add(newFile);
+	ide.onFoldersUpdate();
+	return true;
+    }
+}
+
---
+++ src/main/java/org/luwrain/studio/proj/main/ProjectImpl.java
@@ -0,0 +1,125 @@
+/*
+   Copyright 2012-2021 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.studio.proj.main;
+
+import java.io.*;
+import java.util.*;
+
+import com.google.gson.*;
+import com.google.gson.annotations.*;
+
+import org.luwrain.core.*;
+import org.luwrain.studio.*;
+
+public final class ProjectImpl implements  org.luwrain.studio.Project
+{
+static final Gson gson = new Gson();
+
+    private String projName = null;
+    private Folder folders = null;
+
+    private transient File projFile = null;
+    private transient File projDir = null;
+    private transient IDE ide = null;
+    //    private transient Strings strings = null;
+
+
+    @Override public org.luwrain.studio.Part getPartsRoot() { return folders; }
+    @Override public org.luwrain.studio.Part getMainSourceFile() { return null; }
+
+    @Override public void close()
+    {
+	save();
+    }
+
+    void setProjectFile(File projFile)
+    {
+	final File parent = projFile.getParentFile();
+	if (parent == null)
+	    throw new IllegalArgumentException("projFile must have the not-null parent");
+	this.projDir = parent;
+	this.projFile = projFile;
+    }
+
+    File getProjectDir()
+    {
+	if (projDir == null)
+	    throw new RuntimeException("The project does not have any project directory information");
+	return projDir;
+    }
+
+    private void finalizeLoading(IDE ide)
+    {
+	NullCheck.notNull(ide, "ide");
+	if (folders == null)
+	{
+	    folders = new Folder();
+	    folders.setName("Tex root");
+    }
+	folders.init(this, ide);
+	//	this.strings = (Strings)ide.getLuwrainObj().i18n().getStrings(Strings.NAME);
+    }
+
+
+    public String getProjName()
+    {
+	return this.projName != null?this.projName.trim():"";
+    }
+
+    public void setProjName(String projName)
+    {
+	NullCheck.notNull(projName, "projName");
+	if (projName.trim().isEmpty())
+	    throw new IllegalArgumentException("projName can't be empty");
+	this.projName = projName.trim();
+    }
+
+    public Folder getRootFolder() { return this.folders; }
+
+    public void setRootFolder(Folder rootFolder)
+    {
+	NullCheck.notNull(rootFolder, "rootFolder");
+	this.folders = rootFolder;
+    }
+
+    void save()
+    {
+	if (projFile == null)
+	    throw new IllegalStateException("projFile is not set");
+	try {
+	    try (final BufferedWriter w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(projFile), "UTF-8"))) {
+		gson.toJson(this, w);
+	    }
+	}
+	catch(IOException e)
+	{
+	    throw new RuntimeException(e);
+	}
+    }
+
+    @Override public Project load(File projFile, IDE ide) throws IOException
+    {
+	final Gson gson = new Gson();
+	try (final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(projFile), "UTF-8"))) {
+final ProjectImpl proj = gson.fromJson(reader, ProjectImpl.class);
+proj.setProjectFile(projFile);
+proj.finalizeLoading(ide);
+return proj;
+	}
+    }
+
+}
