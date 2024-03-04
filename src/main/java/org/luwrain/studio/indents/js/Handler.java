77e363b85b4b2dfe1355b7f67355506348330463
Michael Pozhidaev <msp@luwrain.org>
2024-03-04 18:04:52 +0700

    Adding indents.js.Handler

---
+++ src/main/java/org/luwrain/studio/indents/js/Handler.java
@@ -0,0 +1,12 @@
+
+package org.luwrain.studio.indents.js;
+
+public class Handler
+{
+    void beginBlock(String type, int line, int pos)
+    {
+    }
+    void endBlock(String type, int line, int pos)
+    {
+    }
+}
